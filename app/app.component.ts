0d73d6016700aae49a2a1390f5427759168a119e
Loren Glenn <lorencglenn@gmail.com>
2017-01-19 23:10:17 -0800

    create first portion of view

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,36 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Meal Tracker</h1>
+  <ul>
+    <li *ngFor="let currentMeal of meals">{{currentMeal.name}}:
+      <li>{{currentMeal.details}}</li>
+      <li>{{currentMeal.calories}}</li> <button (click)="editMeal()">Edit!</button>
+    </li>
+  </ul>
+  <hr>
+  <div>
+   <h3>{{selectedMeal.name}}</h3>
+  <h3>Edit Meal</h3>
+  <label>Enter Meal Name:</label>
+  <input [(ngModel)]="selectedMeal.name">
+  <label>Enter Meal Details:</label>
+  <input [(ngModel)]="selectedMeal.details">
+  <label>Enter Meal Calories:</label>
+  <input [(ngModel)]="selectedMeal.calories">
+  </div>
+ </div>
   `
 })
 
 export class AppComponent {
-  currentFocus
+  meals: Meal[] = [
+    new Meal('Steak', 'Ribeye 1lb', 800)
+  ];
+  selectedMeal: Meal = this.meals[0];
+
+  editMeal() {
+    alert("edit");
+  }
 }
 
 export class Meal{
